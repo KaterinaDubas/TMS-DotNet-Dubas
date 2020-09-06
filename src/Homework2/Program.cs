e695518b01b03009e8355b7c15e5da551b7391ee
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-09-06 23:52:52 +0300

    feat: solution of homework2

---
+++ src/Homework2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            M: Console.WriteLine("Enter the date to define the day of the week: ");
+            String userDate = Console.ReadLine();
+            bool result = DateTime.TryParse(userDate, out DateTime date);
+            if (result)
+            {
+                Console.WriteLine(date.DayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Date entered incorrectly, try one more time");
+                goto M;
+            }
+        }
+    }
+}
