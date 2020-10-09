76847c37dc85080513e2f0877a8a026dbf116dc6
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-10-09 18:35:46 +0300

    feat: solution of homework 4

---
+++ src/homework4/ToDo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace homework4
+{
+    public class ToDo
+    {
+        private readonly string Id = Guid.NewGuid().ToString().ToUpper().Substring(0, 5);
+        private readonly DateTime _dateTime = DateTime.Now;
+        private ToDoStatus _status = ToDoStatus.Backlog;
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string GetId()
+        {
+            return Id;
+        }
+
+       public void SetStatus(ToDoStatus status)
+        {
+            _status = status;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Status: {_status}");
+            Console.WriteLine($"DateTime: {_dateTime}");
+            Console.WriteLine(Id);
+            Console.WriteLine("-------");
+        }
+
+        public ToDoStatus ConvertStatus (string status)
+        {
+            return status switch
+            {
+                "Backlog" => ToDoStatus.Backlog,
+                "InProgress" => ToDoStatus.InProgress,
+                "Done" => ToDoStatus.Done,
+                "Canceled" => ToDoStatus.Canceled,
+                _ => ToDoStatus.Unknown,
+            };
+        }
+
+
+    }
+}
---
+++ src/homework4/ToDoStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace homework4
+{
+    public enum ToDoStatus
+    {
+        Unknown = 0,
+        Backlog = 1,
+        InProgress = 2,
+        Done = 3,
+        Canceled = 4,
+    }
+}
